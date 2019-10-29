66afe37a6108ea9f0cd17ff3a889d351e4d63fc3
missionkassandra <kassandraerasmofamosomission@gmail.com>
2019-10-29 12:40:21 +0800

    a

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<head>
+	<title>hi</title>
+</head>
 <body>
-
-<!-- Content will go here -->
 
 </body>
 </html>
